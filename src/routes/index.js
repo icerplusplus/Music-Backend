7eb6e1958c85706abf33b7ffd530b0f666ce1d66
Roderick <m.contactqc@gmail.com>
2023-02-12 21:07:11 +0700

    update router ...

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,5 +7,8 @@ const router = express.Router();
 router.get("/genres", musicController.getGenres);
 router.get("/playlist", musicController.getPlaylist);
 router.get("/track", musicController.getTrack);
+router.get("/top-tracks", musicController.getTopTrack);
+router.get("/top-albums", musicController.getTopAlbums);
+router.get("/top-artists", musicController.getTopArtists);
 
 export default router;
