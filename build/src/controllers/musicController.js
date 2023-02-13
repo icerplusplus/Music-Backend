3fdbed3b391760fddc191b34d1a940c7faf0a506
Roderick <m.contactqc@gmail.com>
2023-02-13 13:54:50 +0700

    public router

--- build/src/controllers/musicController.js
+++ build/src/controllers/musicController.js
@@ -10,7 +10,6 @@ var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/de
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 var _axios = _interopRequireDefault(require("axios"));
 var _constant = require("./../libs/constant");
-var _data = require("./../api/data");
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { (0, _defineProperty2["default"])(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 // get Genres
@@ -20,7 +19,7 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 var musicController = {
   getTopTrack: function () {
     var _getTopTrack = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(req, res) {
-      var newOptions;
+      var newOptions, _yield$axios$request, data;
       return _regenerator["default"].wrap(function _callee$(_context) {
         while (1) switch (_context.prev = _context.next) {
           case 0:
@@ -29,11 +28,16 @@ var musicController = {
               params: {
                 region: "VN"
               }
-            }); // const { data } = await axios.request(newOptions);
+            });
+            _context.next = 3;
+            return _axios["default"].request(newOptions);
+          case 3:
+            _yield$axios$request = _context.sent;
+            data = _yield$axios$request.data;
             return _context.abrupt("return", res.status(200).json({
-              data: _data.data.topTrack
+              data: data
             }));
-          case 2:
+          case 6:
           case "end":
             return _context.stop();
         }
@@ -46,7 +50,7 @@ var musicController = {
   }(),
   getTopArtists: function () {
     var _getTopArtists = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee2(req, res) {
-      var newOptions;
+      var newOptions, _yield$axios$request2, data;
       return _regenerator["default"].wrap(function _callee2$(_context2) {
         while (1) switch (_context2.prev = _context2.next) {
           case 0:
@@ -55,11 +59,16 @@ var musicController = {
               params: {
                 region: "VN"
               }
-            }); // const { data } = await axios.request(newOptions);
+            });
+            _context2.next = 3;
+            return _axios["default"].request(newOptions);
+          case 3:
+            _yield$axios$request2 = _context2.sent;
+            data = _yield$axios$request2.data;
             return _context2.abrupt("return", res.status(200).json({
-              data: _data.data.topArtists
+              data: data
             }));
-          case 2:
+          case 6:
           case "end":
             return _context2.stop();
         }
@@ -72,7 +81,7 @@ var musicController = {
   }(),
   getTopAlbums: function () {
     var _getTopAlbums = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee3(req, res) {
-      var newOptions;
+      var newOptions, _yield$axios$request3, data;
       return _regenerator["default"].wrap(function _callee3$(_context3) {
         while (1) switch (_context3.prev = _context3.next) {
           case 0:
@@ -81,11 +90,16 @@ var musicController = {
               params: {
                 region: "VN"
               }
-            }); // const { data } = await axios.request(newOptions);
+            });
+            _context3.next = 3;
+            return _axios["default"].request(newOptions);
+          case 3:
+            _yield$axios$request3 = _context3.sent;
+            data = _yield$axios$request3.data;
             return _context3.abrupt("return", res.status(200).json({
-              data: _data.data.topAlbums
+              data: data
             }));
-          case 2:
+          case 6:
           case "end":
             return _context3.stop();
         }
@@ -98,7 +112,7 @@ var musicController = {
   }(),
   getGenres: function () {
     var _getGenres = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee4(req, res) {
-      var newOptions;
+      var newOptions, _yield$axios$request4, data;
       return _regenerator["default"].wrap(function _callee4$(_context4) {
         while (1) switch (_context4.prev = _context4.next) {
           case 0:
@@ -107,11 +121,16 @@ var musicController = {
               params: {
                 region: "VN"
               }
-            }); // const { data } = await axios.request(newOptions);
+            });
+            _context4.next = 3;
+            return _axios["default"].request(newOptions);
+          case 3:
+            _yield$axios$request4 = _context4.sent;
+            data = _yield$axios$request4.data;
             return _context4.abrupt("return", res.status(200).json({
-              data: _data.data.genres
+              data: data
             }));
-          case 2:
+          case 6:
           case "end":
             return _context4.stop();
         }
@@ -125,7 +144,7 @@ var musicController = {
   // get track list with playlist Id
   getPlaylist: function () {
     var _getPlaylist = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee5(req, res) {
-      var playlistId, newOptions;
+      var playlistId, newOptions, _yield$axios$request5, data;
       return _regenerator["default"].wrap(function _callee5$(_context5) {
         while (1) switch (_context5.prev = _context5.next) {
           case 0:
@@ -135,11 +154,16 @@ var musicController = {
               params: {
                 playlistId: playlistId
               }
-            }); // const { data } = await axios.request(newOptions);
+            });
+            _context5.next = 4;
+            return _axios["default"].request(newOptions);
+          case 4:
+            _yield$axios$request5 = _context5.sent;
+            data = _yield$axios$request5.data;
             return _context5.abrupt("return", res.status(200).json({
-              data: _data.data.playlistTracks
+              data: data
             }));
-          case 3:
+          case 7:
           case "end":
             return _context5.stop();
         }
@@ -154,7 +178,7 @@ var musicController = {
   // find by Track ID, track share URL, track name, ...
   getTrack: function () {
     var _getTrack = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee6(req, res) {
-      var trackId, newOptions;
+      var trackId, newOptions, _yield$axios$request6, data;
       return _regenerator["default"].wrap(function _callee6$(_context6) {
         while (1) switch (_context6.prev = _context6.next) {
           case 0:
@@ -164,11 +188,16 @@ var musicController = {
               params: {
                 track: trackId
               }
-            }); // const { data } = await axios.request(newOptions);
+            });
+            _context6.next = 4;
+            return _axios["default"].request(newOptions);
+          case 4:
+            _yield$axios$request6 = _context6.sent;
+            data = _yield$axios$request6.data;
             return _context6.abrupt("return", res.status(200).json({
-              data: _data.data.track
+              data: data
             }));
-          case 3:
+          case 7:
           case "end":
             return _context6.stop();
         }
