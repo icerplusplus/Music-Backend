3771a395a570f97082fc71ec3caac180b3600635
Roderick <m.contactqc@gmail.com>
2023-03-28 20:05:40 +0700

    update get all favorite list 2

--- build/src/models/FavoritePlaylists.js
+++ build/src/models/FavoritePlaylists.js
@@ -17,6 +17,10 @@ var favoritePlaylistSchema = new _mongoose["default"].Schema({
     required: true,
     maxlength: 30
   },
+  thumbnail: {
+    type: "string",
+    "default": "https://images.unsplash.com/photo-1487180144351-b8472da7d491?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTEwfHxubyUyMG11c2ljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+  },
   songs: {
     type: "array",
     "default": []
